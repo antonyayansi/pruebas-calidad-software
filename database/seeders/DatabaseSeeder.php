f5c14502775bb03ef077555f8c571520d56601a4
EstudianteUC <EstudianteUC@UCUXA40223.continental.edu.pe>
2025-06-10 19:54:50 -0500

    Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $user = new User();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $user->name = "Antony";
+        $user->email = "admin@gmail.com";
+        $user->password = bcrypt("12345678");
+        $user->save();
     }
 }
